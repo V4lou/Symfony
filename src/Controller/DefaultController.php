7c1258193b2345f08f0b8441458c8f57e3bcfe8c
Valentin Fauchère <valentin.fauchere@gmail.com>
2019-11-24 15:31:03 +0100

    Mise à jour quête symfony 5 routing avancé

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,7 +14,7 @@ class DefaultController extends AbstractController
      * @Route("/", name="app_index")
      */
 
-    public function index(): Response
+      public function index(): Response
     {
 
           return $this->render('/index.html.twig');
--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -2,20 +2,47 @@
 // src/Controller/WildController.php
 namespace App\Controller;
 
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\Mapping as ORM;
+
 
 class WildController extends AbstractController
 {
 
     /**
-     * @Route("/wild/show/{slug}", requirements={"slug"="[a-z0\-9]+"},name="wild_show_slug")
+     * @param string $slug The slugger
+     * @Route("/wild/show/{slug}", requirements={"slug"="[a-z0\-9]+"}, defaults={"slug" = null}, name="wild_show_slug")
      */
-    public function show($slug = 'Aucune-série-sélectionnée-veuillez-choisir-une-série.'): Response
+    public function show(?string $slug): Response
     {
         $slugWithoutDash = str_replace("-", " ", $slug);
         $slugInMag = ucwords($slugWithoutDash);
         return $this->render('/wild/show.html.twig', ['slug' => $slugInMag]);
+    }
+    /**
+    * Show all rows from Program’s entity
+    *
+    * @Route("/", name="wild_index")
+    * @return Response A response instance
+    */
+    public function index(): Response
+    {
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+
+        if (!$programs) {
+            throw $this->createNotFoundException(
+                'No program found in program\'s table.'
+            );
+        }
+
+        return $this->render(
+            'wild/index.html.twig',
+            ['programs' => $programs]
+        );
     }
 }
