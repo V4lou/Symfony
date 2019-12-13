54ac207443597bd4562fd6bf2253d7a9f9386b2e
Valentin Fauchère <valentin.fauchere@gmail.com>
2019-12-13 16:42:03 +0100

    ajout fichiers quete 16 2/2

--- src/DataFixtures/ActorFixtures.php
+++ src/DataFixtures/ActorFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Services\Slugify;
 use Faker;
 use App\entity\Category;
 use App\Entity\Program;
@@ -19,20 +20,16 @@ class ActorFixtures extends Fixture implements DependentFixtureInterface
     const ACTORS = ['John Doe', 'Mike Joe', 'Phil Hurston', 'Andrew Marks', 'Jane McVom'];
     public function load(ObjectManager $manager)
     {
-       /* foreach (self::ACTORS as $key => $actorName) {
 
-            $actor = new Actor();
-            $actor->setName($actorName);
-            $actor->addProgram($this->getReference('program_0'));
-            $manager->persist($actor);
-            $this->addReference('actor_' . $key, $actor);
-        }*/
         $faker  =  Faker\Factory::create('en_US');
 
             for ($i = 1; $i <= 50; $i++) {
                 $actor = new Actor();
+                $slug = new Slugify();
                 $actorName = $faker->name;
+                $slug = $slug->generate($actorName);
                 $actor->setName($actorName);
+                $actor->setSlug($slug);
                 $numberProg = rand(0,5);
                 /*for ($j = 0; $j <= 5; $j++) {
                     $actor->addProgram($this->getReference('program_'.$j));
