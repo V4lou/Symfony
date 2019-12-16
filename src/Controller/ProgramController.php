91477a806649ef83dbdc4b73489c2ffa22258053
Valentin Fauchère <valentin.fauchere@gmail.com>
2019-12-16 09:40:24 +0100

    quete 17

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Services\Slugify;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 /**
  * @Route("/program")
@@ -29,7 +31,7 @@ class ProgramController extends AbstractController
     /**
      * @Route("/new", name="program_new", methods={"GET","POST"})
      */
-    public function new(Request $request, Slugify $slugify): Response
+    public function new(Request $request, Slugify $slugify, MailerInterface $mailer): Response
     {
         $program = new Program();
 
@@ -43,7 +45,13 @@ class ProgramController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($program);
             $entityManager->flush();
+            $email = (new Email())
+                ->from('smtp.mailtrap.io')
+                ->to('valentin.fauchere@gmail.com')
+                ->subject('Une nouvelle série vient d\'être publiée !')
+                ->html('<p>Une nouvelle série vient d\'être publiée sur Wild Séries !</p>');
 
+            $mailer->send($email);
             return $this->redirectToRoute('program_index');
         }
 
